8cc70256b9d954831fc7ea13e225ab6f75a06b87
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-04-15 23:52:30 +0300

    edit initDB

--- src/core-ili/db/core-ili-db-init.js
+++ src/core-ili/db/core-ili-db-init.js
@@ -9,47 +9,75 @@ module.exports = {
     {id: '56fe0b15-1844-4d43-9567-6553804463a6', name: 'WorkSpace', description: 'WorkSpace'},
     {id: 'a86fe40e-b6e4-4f2b-b17d-a5f2b4b15134', name: 'SubTasks', description: 'SubTasks'},
   ],
-  clearDBPG: ['prefix_ili_items', 'prefix_ili_links', 'prefix_ili_types_item', 'prefix_ili_types_link', 'prefix_ili_links_rule'],
-  initDBPG: 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";\n' +
-    '\n' +
-    'CREATE TABLE prefix_ili_types_item\n' +
-    '(\n' +
-    '    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),\n' +
-    '    name        VARCHAR(255) NOT NULL,\n' +
-    '    description TEXT\n' +
-    ');\n' +
-    '\n' +
-    'CREATE TABLE prefix_ili_types_link\n' +
-    '(\n' +
-    '    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),\n' +
-    '    name        VARCHAR(255) NOT NULL,\n' +
-    '    description TEXT\n' +
-    ');\n' +
-    '\n' +
-    '\n' +
-    'CREATE TABLE prefix_ili_items\n' +
-    '(\n' +
-    '    id           UUID PRIMARY KEY DEFAULT uuid_generate_v4(),\n' +
-    '    name         VARCHAR(255)                        NOT NULL,\n' +
-    '    description  TEXT,\n' +
-    '    id_type_item UUID references prefix_ili_types_item (id) NOT NULL\n' +
-    ');\n' +
-    '\n' +
-    'CREATE TABLE prefix_ili_links\n' +
-    '(\n' +
-    '    id           UUID PRIMARY KEY DEFAULT uuid_generate_v4(),\n' +
-    '    name         VARCHAR(255)                        NOT NULL,\n' +
-    '    description  TEXT,\n' +
-    '    id_item      UUID references prefix_ili_items (id)      NOT NULL,\n' +
-    '    id_item_1    UUID references prefix_ili_items (id)      NOT NULL,\n' +
-    '    id_item_2    UUID references prefix_ili_items (id)      NOT NULL,\n' +
-    '    id_type_link UUID references prefix_ili_types_link (id) NOT NULL\n' +
-    ');\n' +
-    '\n' +
-    'ALTER TABLE prefix_ili_items\n' +
-    '    ADD COLUMN id_link UUID references prefix_ili_links (id);\n' +
-    '\n'
+  clearDBPG: [
+    'prefix_ili_items',
+    'prefix_ili_links',
+    'prefix_ili_types_item',
+    'prefix_ili_types_link',
+    'prefix_ili_links_rule',
+    'prefix_ili_links_find'
+  ],
+  initDBPG: `
+    
+
+    CREATE TABLE prefix_ili_types_item
+    (
+      id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name        VARCHAR(255) NOT NULL,
+      description TEXT
+    );
+
+    CREATE TABLE prefix_ili_types_link
+    (
+      id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name        VARCHAR(255) NOT NULL,
+      description TEXT
+    );
+
+    CREATE TABLE prefix_ili_items
+    (
+      id           UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name         VARCHAR(255)                        NOT NULL,
+      description  TEXT,
+      params       json,
+      id_type_item UUID references prefix_ili_types_item (id) NOT NULL
+    );
+
+    CREATE TABLE prefix_ili_links
+    (
+      id           UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name         VARCHAR(255)                        NOT NULL,
+      description  TEXT,
+      id_item      UUID references prefix_ili_items (id)      NOT NULL,
+      id_item_1    UUID references prefix_ili_items (id)      NOT NULL,
+      id_type_item_1  UUID references prefix_ili_types_item (id) NOT NULL
+      id_item_2    UUID references prefix_ili_items (id)      NOT NULL,
+      id_type_item_2  UUID references prefix_ili_types_item (id) NOT NULL
+      id_type_link UUID references prefix_ili_types_link (id) NOT NULL
+    );
+
+    ALTER TABLE prefix_ili_items
+      ADD COLUMN id_link UUID references prefix_ili_links (id);
+
+    CREATE TABLE prefix_ili_links_find
+    (
+      id              UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      name            VARCHAR(255)                        NOT NULL,
+      description     TEXT,
+      id_item         UUID references prefix_ili_items (id)      NOT NULL,
+      id_type_item    UUID references prefix_ili_types_item (id) NOT NULL
+      id_item_1       UUID references prefix_ili_items (id)      NOT NULL,
+      id_type_item_1  UUID references prefix_ili_types_item (id) NOT NULL
+      id_item_2       UUID references prefix_ili_items (id)      NOT NULL,
+      id_type_item_2  UUID references prefix_ili_types_item (id) NOT NULL
+      id_link         UUID references prefix_ili_links (id)      NOT NULL,
+      id_type_link    UUID references prefix_ili_types_link (id) NOT NULL
+    );
+  `
 }
+
+
+// CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 // +
 // 'CREATE TABLE prefix_ili_links_rule\n' +
